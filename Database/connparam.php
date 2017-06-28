50aa8257cc1abd088b8a09388131e7e4d2a40a4a
LucaStorer <lucastorer86@gmail.com>
2017-06-28 16:30:17 +0200

    First Commit 2.0

---
+++ Database/connparam.php
@@ -0,0 +1,8 @@
+<?php
+
+$servername = "localhost";
+$username = "u760134217_luca";
+$password = "P@wcallu86";
+$dbname = "u760134217_mf";
+
+ ?>
--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php   
-session_start(); //to ensure you are using same session
-session_destroy(); //destroy the session
-header("location: MyFuelLogin.php"); //to redirect back to "test.php" after logging out
-exit();
-?>
